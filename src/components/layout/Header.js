8c676f643c2a414260a6b8c8f53fccf2784364dd
App++ <43849498+AppPlusx2@users.noreply.github.com>
2023-02-12 10:00:00 +0800

    Converted px to rem & implemented BEM

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,28 +8,34 @@ function Header() {
     <header className={classes.header}>
       {/* we put the image inside the div so that we have a container that we can
       format that contain our logo */}
-      <div className={classes.logo_box}>
-        <img src={logo} alt="Logo" className={classes.logo}></img>
-        <img src={logoName} alt="LogoName" className={classes.logo}></img>
+      {/*con la nomenclatura BEM, header is the block and logo box is the element*/}
+      <div className={classes["header__logo-box"]}>
+        <img src={logo} alt="Logo" className={classes.header__logo}></img>
+        <img
+          src={logoName}
+          alt="LogoName"
+          className={classes.header__logo}
+        ></img>
       </div>
 
-      <div className={classes.button_box}>
+      <div className={classes["header__button-box"]}>
         {/*The anchor tag in HTML is used to navigate to different web pages using an href attribute, wew use it instead of the button element*/}
+        {/*The button we can consider as a standalone block because we are going to use it throughout the code in other parts*/}
         <a
           href="a"
-          className={`${classes.btn} ${classes.btn_white} ${classes.btn_animation}`}
+          className={`${classes.btn} ${classes["btn--white"]} ${classes["btn--animation"]}`}
         >
           Bottone
         </a>{" "}
       </div>
 
       {/* we put the text inside the div so that we can center all in the middle of the header */}
-      <div className={classes.text_box}>
+      <div className={classes["header__text-box"]}>
         <h1 className={classes.heading_primary}>
-          <span className={classes.heading_primary_main}>
+          <span className={classes["heading_primary--main"]}>
             Decentralized Contracts
           </span>
-          <span className={classes.heading_primary_sub}>
+          <span className={classes["heading_primary--sub"]}>
             Leveraging blockchain technologies within the supply-chain
           </span>
         </h1>
