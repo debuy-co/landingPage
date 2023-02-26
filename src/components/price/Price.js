2d2985c3f6f5a88eef337148484a05fca2b6e478
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-02-27 07:49:08 +0800

    Building the Tours Section - Part 2

--- src/components/price/Price.js
+++ src/components/price/Price.js
@@ -9,7 +9,24 @@ function Price() {
       <div className="row">
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">FRONT</div>
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--1">
+                &nbsp; {/*this in HTML means a white space*/}
+              </div>
+              <h4 className="card__heading">
+                {/*this span here is needed in order to give the box decoration effet to the heading*/}
+                <span className="card__heading-span card__heading-span--1">
+                  for individuals
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>option 1</li>
+                  <li>option 2</li>
+                  <li>option 3</li>
+                </ul>
+              </div>
+            </div>
             <div className="card__side card__side--back card__side--back-1">
               BACK
             </div>
@@ -17,7 +34,13 @@ function Price() {
         </div>
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">FRONT</div>
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--2">
+                &nbsp; {/*this in HTML means a white space*/}
+              </div>
+              <h4 className="card__heading">for small teams</h4>
+              <div className="card__details">Details</div>
+            </div>
             <div className="card__side card__side--back card__side--back-2">
               BACK
             </div>
@@ -25,7 +48,13 @@ function Price() {
         </div>
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">FRONT</div>
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--3">
+                &nbsp; {/*this in HTML means a white space*/}
+              </div>
+              <h4 className="card__heading">for big organizations</h4>
+              <div className="card__details">Details</div>
+            </div>
             <div className="card__side card__side--back card__side--back-3">
               BACK
             </div>
