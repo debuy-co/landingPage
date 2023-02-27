84f9f44b5476f12be6ab5cc173a57e331093d71c
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-02-28 06:47:43 +0800

    Building the Tours Section - Part 3

--- src/components/price/Price.js
+++ src/components/price/Price.js
@@ -28,7 +28,17 @@ function Price() {
               </div>
             </div>
             <div className="card__side card__side--back card__side--back-1">
-              BACK
+              {/*cta stands for call to action*/}
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">$297</p>
+                </div>
+                {/*btn--white is the same scss element that we styled when using the header*/}
+                <a href="#1" className="btn btn--white btn--animation">
+                  Book now
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -38,11 +48,30 @@ function Price() {
               <div className="card__picture card__picture--2">
                 &nbsp; {/*this in HTML means a white space*/}
               </div>
-              <h4 className="card__heading">for small teams</h4>
-              <div className="card__details">Details</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--2">
+                  for small teams
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>option 1</li>
+                  <li>option 2</li>
+                  <li>option 3</li>
+                </ul>
+              </div>
             </div>
             <div className="card__side card__side--back card__side--back-2">
-              BACK
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">$xxx</p>
+                </div>
+                {/*btn--white is the same scss element that we styled when using the header*/}
+                <a href="#1" className="btn btn--white btn--animation">
+                  Book now
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -52,14 +81,40 @@ function Price() {
               <div className="card__picture card__picture--3">
                 &nbsp; {/*this in HTML means a white space*/}
               </div>
-              <h4 className="card__heading">for big organizations</h4>
-              <div className="card__details">Details</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--3">
+                  for big organizations
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>option 1</li>
+                  <li>option 2</li>
+                  <li>option 3</li>
+                </ul>
+              </div>
             </div>
             <div className="card__side card__side--back card__side--back-3">
-              BACK
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">$xxx</p>
+                </div>
+                {/*btn--white is the same scss element that we styled when using the header*/}
+                <a href="#1" className="btn btn--white btn--animation">
+                  Book now
+                </a>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+
+      {/*//TODO: vedere perche anhe in questo caso non e' centrato*/}
+      <div className="u-center-text u-margin-bottom-big">
+        <a href="#2" className="btn btn--white">
+          Button another
+        </a>
       </div>
     </section>
   );
