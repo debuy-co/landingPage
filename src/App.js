3f80b860e9082639379413429f6a9bd21d376b84
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-02-28 08:18:42 +0800

    Building the Testimonials Section - Part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/layout/Header";
 import About from "./components/about/About";
 import Features from "./components/features/Features";
 import Price from "./components/price/Price";
+import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Features />
       <Price />
+      <Testimonials />
       {/* <GridTest /> //this was only to show the grid layout*/}
     </div>
   );
---
+++ src/components/testimonials/Testimonials.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ImageWhite from "../../assets/img/block-grey.jpg";
+
+function Testimonials() {
+  return (
+    <section className="section-testimonials">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">What they say about us</h2>
+      </div>
+
+      <div className="row">
+        <div className="testimonial">
+          {/*semantically the figure is the best thing to use in this case as we are giong to have an image with some text*/}
+          <figure className="testimonial__shape">
+            <img src={ImageWhite} alt="prova" className="testimonial__img" />
+          </figure>
+          <div className="testimonial__text">
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Lorem ipsum
+            </h3>
+            <p>
+              dolor sit amet consectetur adipisicing elit. Voluptatem eveniet
+              accusamus hic, facilis sunt autem, quos nulla est nam praesentium
+              dolores assumenda magnam, illo eaque quibusdam unde ea vero
+              necessitatibus
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
