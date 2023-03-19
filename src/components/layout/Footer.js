1e8fb7c8ea47ada97650958f901f8483008ac6cb
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-03-19 19:17:03 +0800

    Responsive Images: ArtDirection, DensitySwitching

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,37 @@
 import React from "react";
-import fullLogo from "../../assets/logo/full_transparent.png";
+import fullLogoHigh from "../../assets/logo/full_transparent_high.png";
+import fullLogoLow from "../../assets/logo/full_transparent_low.png";
+import logo from "../../assets/logo/logoname_transparent.png";
 
 function Footer() {
+  let logoDensitySwitching = `${fullLogoHigh} 2x, ${fullLogoLow} 1x`;
   return (
     <footer className="footer">
       <div className="footer__logo-box">
-        <img src={fullLogo} alt="fullLogo" className="footer__logo"></img>
+        {/* <img
+          //--> here we use the responsive image property - Density Switching
+          srcSet={logoDensitySwitching}
+          alt="fullLogo"
+          className="footer__logo"
+        ></img> */}
+
+        <picture
+        //--> here we use the responsive image property - Art Direction
+        //It means that we are going to tell the browser to use one image with one screen and another image with another screen width, so we want to make images depend on the screen width
+
+        //what is happening below is basically to use different sources for different cases, if the screen is below 37.5em == 600px, than we use the images that are inside <source/> otherwise we use the images that are inside <img>
+        >
+          <source
+            srcset={logo} //even if it is not a set "source" require scrset attribute
+            media="(max-width: 37.5em)"
+            className="footer__logo"
+          ></source>
+          <img
+            srcSet={logoDensitySwitching}
+            alt="fullLogo"
+            className="footer__logo"
+          ></img>
+        </picture>
       </div>
       <div className="row">
         <div className="col-1-of-2">
